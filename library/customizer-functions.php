046837c74147023d6260db8c9fb79093723460c2
nitrokevin <kevinneal@macbook-pro.home>
2020-02-27 19:38:20 +0000

    customizer nav colour

--- library/customizer-functions.php
+++ library/customizer-functions.php
@@ -62,7 +62,7 @@ function avidd_theme_settings( $wp_customize ) {
       ],     
       'output'      => array(
         array(
-          'element'  => ' .top-bar',
+          'element'  => ' .top-bar, .top-bar ul',
           'property' => 'background-color'
         ),
       ));
